3442f8a5b20c8f5c66e6b66393812f7a1f4b99b4
GC | クイン ー Vincent <quyen@gcrest.com>
2015-10-19 21:31:07 +0900

    creat file store directory on server

---
+++ Backend/luvbox-service/src/main/java/service/creatServerLocationService/createServerLocationService.java
@@ -0,0 +1,8 @@
+package service.creatServerLocationService;
+
+/**
+ * Created by m00246-quyen on 10/19/15.
+ */
+public interface createServerLocationService {
+    public String excute();
+}
---
+++ Backend/luvbox-service/src/main/java/service/creatServerLocationService/createServerLocationServiceImpl.java
@@ -0,0 +1,10 @@
+package service.creatServerLocationService;
+
+/**
+ * Created by m00246-quyen on 10/19/15.
+ */
+public class createServerLocationServiceImpl implements createServerLocationService{
+    public String excute(){
+        return null;
+    }
+}
